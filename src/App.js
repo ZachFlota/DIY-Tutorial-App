81d0da64604d0c88bc799d727d3b6dc05594c3fc
Rocco Mannino <RWMannio@gmail.com>
2022-06-04 14:23:43 -0400

    early stage navbar w/search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import { useState } from 'react'
+import React from 'react';
+import { useEffect ,useState } from 'react';
+import Navbar from './components/navbar';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>DIY Tutorial App</h1>
+        <Navbar />
       </header>
     </div>
   );
---
+++ src/components/home.jsx
@@ -0,0 +1,7 @@
+const React = require('react');
+
+function home() {
+  return <h1>Home</h1>;
+}
+
+module.exports = home;
