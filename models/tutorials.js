bded4f96133dde613654948ee268b67f4d43cf49
Zachary Flota <zflota1@gmail.com>
2022-06-04 14:12:17 -0400

    createGuide form sample

--- models/tutorials.js
+++ models/tutorials.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose')
 
 const tutorialSchema = new mongoose.Schema({
     title: { type: String, required: true},
+    description: { type: String },
+    step: { type: String },
+    image: { type: String },
     
 })
 
--- src/components/CreateGuide.js
+++ src/components/CreateGuide.js
@@ -1,11 +1,34 @@
 const React = require('react')
 
 function CreateGuide(){
+    
+
+    function addStep() {
+       
+        var newLabel = document.createElement('label')
+        newLabel.innerHTML = 'Step'
+
+        var newInput = document.createElement('input');
+        newInput.setAttribute('type', 'text')
+    
+        var newImageLabel = document.createElement('label')
+        newImageLabel.innerHTML = 'Image'
+
+        var newImage = document.createElement('input');
+        newImage.setAttribute('type', 'text')
+
+        document.getElementById("newStep").appendChild(newLabel);
+        document.getElementById("newStep").appendChild(newInput);
+        document.getElementById("newStep").appendChild(newImageLabel);
+        document.getElementById("newStep").appendChild(newImage);
+        
+    }
+
     return (
         <main>
             <h1>Create a Tutorial!</h1>
             <form method="POST" action="/tutorial">
-                <div classname="form-group">
+                <div className="form-group">
                     <label htmlFor="title">Title</label>
                     <input className="form-control" id="title" name="title" required />
                 </div>
@@ -18,8 +41,15 @@ function CreateGuide(){
                     <input className="form-control" id="step" name="step" required />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="image">Step Image</label>
+                    <label htmlFor="image">Image 1</label>
                     <input className="form-control" id="image" name="image" />
+                </div>
+                <div id="newStep"></div>
+                <div>
+                    <input type="button" value="Add Step" onClick={addStep} /> 
+                </div>
+                <div>
+                    <input type="submit" className="btn btn-primary" value="Create Tutorial"></input>
                 </div>
                 
 
